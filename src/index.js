b6bf8a0b7509c3dd89a68c03a1ec1f4970c654c6
Luciana Blümlein <luziehtan@hotmail.com>
2021-02-04 14:36:58 +0100

    Add Card files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import Header from './components/Header'
 import Button from './components/Button'
 
-const header = Header()
-
+const header = Header('Quiz App', 'May the best win!')
 document.body.append(header)
 
 const button = Button('Click me!')
-
+const button2 = Button('Hold me')
 document.body.append(button)
+document.body.append(button2)
